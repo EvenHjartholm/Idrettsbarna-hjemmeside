275270e0dbc94e6900f13675f0abe770d82dc364
Even Hjartholm <evenhjartholm@hotmail.com>
2025-11-26 09:32:29 +0100

    Add redirect for /news to /nyheter

--- App.tsx
+++ App.tsx
@@ -46,6 +46,7 @@ const App: React.FC = () => {
           <Route path="/pameldingkontakt" element={<Navigate to="/" replace />} />
           <Route path="/babysvomming-asker" element={<Navigate to="/" replace />} />
           <Route path="/news/babysvomming-i-asker-med-idrettsbarna---oppst-12" element={<Navigate to="/nyheter/babysvomming-i-asker-med-idrettsbarna---oppst-12" replace />} />
+          <Route path="/news" element={<Navigate to="/nyheter" replace />} />
           <Route path="/nyheter" element={<NewsPage />} />
           <Route path="/nyheter/:slug" element={<NewsArticlePage />} />
         </Routes>
