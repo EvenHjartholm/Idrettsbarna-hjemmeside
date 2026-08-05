04d01771284b94edb3c47cce8c37796a59085e7e
Even Hjartholm <evenhjartholm@hotmail.com>
2026-08-05 11:01:57 +0200

    Baby nybegynner tor 13:10 satt til venteliste

--- constants.ts
+++ constants.ts
@@ -50,7 +50,7 @@ export const SCHEDULE_DATA: ScheduleDay[] = [
     sessions: [
       { time: "---", level: "RISENGA VARMTVANNSSBASSENG", ageGroup: "34 grader" },
       { time: "12:45 - 13:10", level: "Babysvømming", ageGroup: "Nybegynner og Øvet", serviceId: "baby", spots: "Venteliste" },
-      { time: "13:10 - 13:35", level: "Babysvømming", ageGroup: "Nybegynner", serviceId: "baby", spots: 2 },
+      { time: "13:10 - 13:35", level: "Babysvømming", ageGroup: "Nybegynner", serviceId: "baby", spots: "Venteliste" },
       { time: "13:35 - 14:00", level: "Babysvømming", ageGroup: "Nybegynner", serviceId: "baby", spots: "Venteliste" },
       { time: "---", level: "RISENGA VARMTVANNSSBASSENG", ageGroup: "Ettermiddag/Kveld" },
       { time: "15:00 - 15:30", level: "Babysvømming", ageGroup: "Nybegynner", serviceId: "baby", spots: "Venteliste" },
